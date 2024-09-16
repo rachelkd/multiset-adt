7e02d23a417734061586f19211567b89ee85694f
rachelkd <rachel05deng@gmail.com>
2024-09-16 17:05:53 -0400

    Implemented Tree constructors

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,17 @@
+import java.util.List;
+
 public class Tree {
+    private List<Tree> subtrees;
+    private int root;
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+    public Tree(int root, List<Tree> subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
+    public Tree(int root) {
+        this.root = root;
+        this.subtrees = new List<Tree>();
+    }
 }
